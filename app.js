b58aabcdf5228d4703abef4ecc2a924548a1c8df
manimaran003 <manimaranc@dorustree.in>
2022-08-21 18:53:27 +0530

    url

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PatientRouter=require('./routes/PatientRoutes')
 const app=express()
 const cors = require('cors');
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://abc-hospital-demo.vercel.app', 
     credentials:true,          
     optionSuccessStatus:200
 }
